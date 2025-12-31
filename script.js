33a6fc2e72c404924e535328cff2565728743e47
RamdaniBagus <ramdanibagus24@gmail.com>
2025-12-31 17:19:33 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ var icon = document.getElementById(icon);
+ icon.onclick = function(){
+    document.body.toggle.icon("dark-mode");
+ }
+
+function displaynum(n1){
+    Calculator.text1.value=Calculator.text1.value+n1;
+}
